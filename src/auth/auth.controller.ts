b6660e0e7e9898d4fd1dc01ec39f91da58861820
YigitSelcuk <yigitselcuk2910@gmail.com>
2026-03-25 12:20:16 +0300

    fixed-auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
-import { Throttle } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { LoginDto } from './login.dto';
 
@@ -7,8 +6,6 @@ import { LoginDto } from './login.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // Login: max 10 deneme / 60 saniye
-  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @Post('login')
   async login(@Body() body: LoginDto) {
     const { username, password } = body;
